5b8603b6e67da9146151817b1a81aaffb80508dd
chun20042 <chun20042@gmail.com>
2013-10-19 16:22:10 +0800

    add the random color

--- main.js
+++ main.js
@@ -4,13 +4,26 @@
     timeout=0;
 
     function start()
-    {var myCanvas =document.getElementById("myCanvas");
+    {function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+
+
+
+      var myCanvas =document.getElementById("myCanvas");
   var myContext=myCanvas.getContext("2d");
 myCanvas.height=600;
 myCanvas.width=600;
   var size= Math.random();
-  myContext.strokeStyle="black";
-  myContext.fillStyle="black";
+  var color1=get_random_color();
+  myContext.strokeStyle=color1;
+  myContext.fillStyle=color1;
 
   myContext.beginPath();
   myContext.arc(75*size,75*size,40*size,Math.PI*1/2,Math.PI*-1/2,false);
@@ -34,7 +47,7 @@ myCanvas.width=600;
   myContext.arc(75*size,55*size,5*size,0,Math.PI*2,true);
   myContext.closePath();
   myContext.fill();
-  myContext.fillStyle="black";
+  myContext.fillStyle=color1;
   myContext.beginPath();
   myContext.arc(75*size,95*size,5*size,0,Math.PI*2,true);
   myContext.closePath();
