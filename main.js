a12c76971cc464120327e60fde36b04ae6de6a34
chun20042 <chun20042@gmail.com>
2013-10-19 14:46:52 +0800

    fix #7

---
+++ main.js
@@ -0,0 +1,40 @@
+
+  (function()
+    {var myCanvas =document.getElementById("myCanvas");
+  var myContext=myCanvas.getContext("2d");
+
+  var size= Math.random();
+  myContext.strokeStyle="black";
+  myContext.fillStyle="black";
+
+  myContext.beginPath();
+  myContext.arc(75*size,75*size,40*size,Math.PI*1/2,Math.PI*-1/2,false);
+  myContext.closePath();
+  myContext.fill();
+  myContext.beginPath();
+  myContext.arc(75*size,55*size,20*size,Math.PI*1/2,Math.PI*-1/2,true);
+  myContext.fill();
+  myContext.fillStyle="white";
+  myContext.beginPath();
+  myContext.arc(75*size,95*size,20*size,Math.PI*1/2,Math.PI*-1/2,false);
+  myContext.fill();
+
+  myContext.beginPath();
+  myContext.arc(75*size,75*size,40*size,0,Math.PI*2,true);
+  myContext.closePath();
+  myContext.stroke();
+  myContext.fillStyle="white";
+  myContext.beginPath();
+
+  myContext.arc(75*size,55*size,5*size,0,Math.PI*2,true);
+  myContext.closePath();
+  myContext.fill();
+  myContext.fillStyle="black";
+  myContext.beginPath();
+  myContext.arc(75*size,95*size,5*size,0,Math.PI*2,true);
+  myContext.closePath();
+  myContext.fill();
+
+
+  })();
+
