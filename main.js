0d10a9e81e89a16742d8876406e1212012e0ebf5
chun20042 <chun20042@gmail.com>
2013-10-20 11:54:52 +0800

    fix #9 #10 #11

--- main.js
+++ main.js
@@ -1,78 +1,90 @@
 
   var game=(function(){
     var count=0,
-    timeout=0;
-
-    function start()
-    {function get_random_color() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.round(Math.random() * 15)];
-    }
-    return color;
+    timeout,
+    x,
+    y,
+    size,
+    mark=0;
+    
+function start(){
+  document.getElementById("main").addEventListener("click",clickEvent,false);
+startGame();
 }
 
+function get_random_color() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+            for (var i = 0; i < 6; i++ ) {
+                color += letters[Math.round(Math.random() * 15)];
+            }
+        return color;
+     }
+
+function clickEvent(evt){
+var xx=evt.clientX;
+var yy=evt.clientY;
+ 
+var tmp =(x-xx)*(x-xx)+(y-yy)*(y-yy);
+
+if(tmp<=size*size){
+  var score= 100-size;
+  mark += score;
+  console.log("clicked"+score);
+  }
 
 
-
-      var myCanvas =document.getElementById("myCanvas");
-  var myContext=myCanvas.getContext("2d");
-myCanvas.height=600;
-myCanvas.width=600;
-  var size= Math.random();
-  var color1=get_random_color();
-  myContext.strokeStyle=color1;
-  myContext.fillStyle=color1;
-
-  myContext.beginPath();
-  myContext.arc(75*size,75*size,40*size,Math.PI*1/2,Math.PI*-1/2,false);
-  myContext.closePath();
-  myContext.fill();
-  myContext.beginPath();
-  myContext.arc(75*size,55*size,20*size,Math.PI*1/2,Math.PI*-1/2,true);
-  myContext.fill();
-  myContext.fillStyle="white";
-  myContext.beginPath();
-  myContext.arc(75*size,95*size,20*size,Math.PI*1/2,Math.PI*-1/2,false);
-  myContext.fill();
-
-  myContext.beginPath();
-  myContext.arc(75*size,75*size,40*size,0,Math.PI*2,true);
-  myContext.closePath();
-  myContext.stroke();
-  myContext.fillStyle="white";
-  myContext.beginPath();
-
-  myContext.arc(75*size,55*size,5*size,0,Math.PI*2,true);
-  myContext.closePath();
-  myContext.fill();
-  myContext.fillStyle=color1;
-  myContext.beginPath();
-  myContext.arc(75*size,95*size,5*size,0,Math.PI*2,true);
-  myContext.closePath();
-  myContext.fill();
-
-      if(count <10){
-      timeout=setTimeout(start,1000);
-      count++;
- console.log(timeout+"&"+count);
-      }
-
-
-
-}
-return{
-  start:start
 }
 
 function gameover(){
-
- 
-
+  console.log("Final:"+mark);
 
 }
 
+
+    function startGame()
+      {
+
+
+
+
+        var myCanvas =document.getElementById("myCanvas");
+    var myContext=myCanvas.getContext("2d");
+  myCanvas.height=800;
+  myCanvas.width=800;
+     size= Math.random()*100;
+       x= Math.random()*800;
+         y= Math.random()*800 ;
+    var color1=get_random_color();
+    myContext.strokeStyle=color1;
+    myContext.fillStyle=color1;
+
+    myContext.beginPath();
+    myContext.arc(x,y,size,Math.PI*2,0,false);
+    myContext.closePath();
+    myContext.fill();
+    
+
+        if(count <10){
+          count++;
+        timeout=setTimeout(startGame,1500);
+        
+   console.log(timeout+"&"+count);
+        }
+        else{
+          gameover();
+        }
+
+
+
+  }
+    
+
+    return{
+        start:start
+          }
+
+  
 
 
 })();
